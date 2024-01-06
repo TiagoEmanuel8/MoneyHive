672f27474a3a7b59eb7d4989fce712e26d8b2b38
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2024-01-06 19:53:31 -0300

    commit inicial

---
+++ src/main/java/com/example/moneyhive/MoneyhiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.moneyhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyhiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyhiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moneyhive/MoneyhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneyhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
